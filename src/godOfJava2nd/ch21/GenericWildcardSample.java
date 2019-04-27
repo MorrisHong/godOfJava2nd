ef49acdd0835a0f86263787a59bf0916847e74a4
govlmo91 <govlmo91@192.168.0.107>
2019-04-28 04:18:26 +0900

    Generic Method learn

---
+++ src/godOfJava2nd/ch21/GenericWildcardSample.java
@@ -0,0 +1,19 @@
+package godOfJava2nd.ch21;
+
+public class GenericWildcardSample {
+	public static void main(String[] args) {
+		GenericWildcardSample sample = new GenericWildcardSample();
+		sample.callGenericMethod();
+	}
+	
+	public <T> void genericMethod(WildcardGeneric<T> c, T addValue) {
+		c.setWildcard(addValue);
+		T value = c.getWildcard();
+		System.out.println(value);
+	}
+	
+	public void callGenericMethod() {
+		WildcardGeneric<String> wildcard = new WildcardGeneric<>();
+		genericMethod(wildcard, "eunmo");
+	}
+}
