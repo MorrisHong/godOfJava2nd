507b1ca1ca0505d67fc077d93f909714988be863
govlmo91 <govlmo91@192.168.0.107>
2019-04-28 21:32:41 +0900

    ch25(Thread) init.

---
+++ src/godOfJava2nd/ch25/RunThreads.java
@@ -0,0 +1,18 @@
+package godOfJava2nd.ch25;
+
+public class RunThreads {
+	public static void main(String[] args) {
+		RunThreads threads = new RunThreads();
+		threads.runBasic();
+	}
+	
+	public void runBasic() {
+		RunnableSample runnable = new RunnableSample();
+		new Thread(runnable).start();
+		
+		ThreadSample thread = new ThreadSample();
+		thread.start();
+		
+		System.out.println("RunThreads.runBasic() method is ended");
+	}
+}
---
+++ src/godOfJava2nd/ch25/ThreadSample.java
@@ -0,0 +1,8 @@
+package godOfJava2nd.ch25;
+
+public class ThreadSample extends Thread{
+	@Override
+	public void run() {
+		System.out.println("This is ThreadSample's rum() method.");
+	}
+}
